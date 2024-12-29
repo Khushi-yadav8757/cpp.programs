3e3d8301f3384a190c7d40279c4f9025ff6dcf8e
Khushi-yadav8757 <kky957242@gmail.com>
2024-12-29 22:26:17 +0530

    sum

---
+++ sum1.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main (){
+    int x,y; 
+    cout<<"enter first number :";
+    cin>> x;
+    cout<<"enter the second number :";
+    cin>> y;
+    cout<<"sum of the number is:" << x+y;
+    return 0;
+}
